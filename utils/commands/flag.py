3290a33ad5949eded57296d2cc312fb51c75e407
LuskaBol <lucas@educatux.com.br>
2021-10-31 20:11:13 -0300

    tentando fazer o bagulho de verificar a flag cafunfar

--- utils/commands/flag.py
+++ utils/commands/flag.py
@@ -1,5 +1,8 @@
+from ..database.setup import Attempt
+
+
 def check_flag(challId, flag, userId):
-    pass
+    return Attempt.select().where(Attempt.correct == True, Attempt.userId == userId, Attempt.chall_id == challId)
     # Precisa verificar se o usuário já fez essa chall
     # Precisa verificar a flag
     # Se a flag for correta, marcar como correto
