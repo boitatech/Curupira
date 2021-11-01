8468344459cc051a79c6236f6be532336766c56b
Afonso Coutinho <afonso@yack.com.br>
2021-11-01 03:05:08 -0300

    fix ranking

--- utils/commands/rank.py
+++ utils/commands/rank.py
@@ -2,17 +2,18 @@ import utils.logging.log as log
 from ..database.setup import User, Attempt
 
 
-def get_ranking_with_user(ctx):
+def get_ranking_top_ten():
     try:
         users = (
             User.select()
             .order_by(User.score.desc())
             .order_by(User.last_submit.desc())
+            .limit(10)
             )
 
         ranking = ""
         for idx, user in enumerate(users.iterator()):
-            ranking += f'''#{idx} <@{user.discordId}> - {user.score}
+            ranking += f'''#{idx+1} <@{user.discordId}> - {user.score}
 ---------------------------------------
 '''
         return ranking
