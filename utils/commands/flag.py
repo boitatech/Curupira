7758d9b2d68c0f0c5bd5225844267684421bd1f4
LuskaBol <lucas@educatux.com.br>
2021-11-01 14:49:47 -0300

    fazendo funcionar a submissao de flags

--- utils/commands/flag.py
+++ utils/commands/flag.py
@@ -1,10 +1,10 @@
 import utils.logging.log as log
 
-from ..database.setup import Attempt, Challenge
+from ..database.setup import Attempt, Challenge, User
 import peewee
 
 
-def check_flag(challId, flag, userId):
+def check_flag(challId, flag, discordId):
     """
     Essa funcao verifica se a flag esta correta, criando sempre um Attempt
 
@@ -14,13 +14,18 @@ def check_flag(challId, flag, userId):
     :userId => ID do Usuario
     """
     try:
+        user = User.get(User.discordId == discordId)
+    except:
+        return "Você não está cadastrado, use o comando $register para se cadastrar!"
+
+    try:
         chall = Challenge.get_by_id(challId)
     except Exception as e:
         log.err(e)
         return "Esse challenge id não existe!"
 
     try:
-        Attempt.get(Attempt.correct == True, Attempt.user_id == userId, Attempt.chall_id == challId)
+        Attempt.get(Attempt.correct == True, Attempt.user_id == user.id, Attempt.chall_id == challId)
         return "Você já submeteu a flag desse desafio!"
     except peewee.DoesNotExist as e:
         log.err(e)
@@ -30,14 +35,14 @@ def check_flag(challId, flag, userId):
 
     if chall.flag == flag:
         try:
-            Attempt.create(user_id=1, chall_id=1, flag=flag, correct=True)
+            Attempt.create(user_id=user.id, chall_id=challId, flag=flag, correct=True)
             return "Flag correta!"
         except Exception as e:
             log.err(e)
             return "Erro ao criar Attempt True"
     else:
         try:
-            Attempt.create(user_id=1, chall_id=1, flag=flag, correct=False) 
+            Attempt.create(user_id=user.id, chall_id=challId, flag=flag, correct=False) 
             return "Flag incorreta!"
         except Exception as e:
             log.err(e)
