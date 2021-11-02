37e8a1e2d2092b2b4218dc75cc3281d282923ce9
Luidiblu <diego.luidiblu@gmail.com>
2021-11-02 01:09:08 -0300

    embeded return challs

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ async def challs(ctx):
     """
 
     await ctx.send("\n============== CHALLENGES ==============")
-    await ctx.send(get_challenges())
+    await ctx.send(embed=get_challenges())
 
 @bot.group(invoke_without_command=True)
 async def help(ctx):
