4d4ff425cbbf96106f8f01178f907116d21dbc45
LuskaBol <lucas@educatux.com.br>
2021-11-02 01:55:10 -0300

    enviando as challs pela dm

--- utils/commands/challenge.py
+++ utils/commands/challenge.py
@@ -1,21 +1,22 @@
-from ..database.setup import Challenge, Attempt
+from ..database.setup import Challenge
 import utils.logging.log as log
 import discord
 
-def get_challenges(ctx):
+def get_challenges():
     """
     Essa funcao retorna todos os challenges cadastrados no CTF
     """
     try:
-        challenges = Challenge.select(Challenge.id,
-                                      Challenge.name, 
-                                      Challenge.points, 
-                                      Challenge.category, 
-                                      Challenge.description,
-                                      Challenge.url).join(Attempt, on=(Challenge.id == Attempt.chall_id)).where(Attempt.correct == False)
-        challs = ""
-        for challenge in challenges.iterator(): 
-            challs += f'''{challenge.name} ({challenge.id}) - {challenge.points} Pontos - {challenge.category}
+        challenges = Challenge.select(
+            Challenge.id,
+            Challenge.name, 
+            Challenge.points, 
+            Challenge.category, 
+            Challenge.description,
+            Challenge.url
+            )
+        challs = "".join(
+            f'''{challenge.name} ({challenge.id}) - {challenge.points} Pontos - {challenge.category}
                           {challenge.description}
                           {challenge.url}
                        ------------------------------------------------------------
