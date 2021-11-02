72b715b78d74bd0faae2df7af59438486251fe40
Luidiblu <diego.luidiblu@gmail.com>
2021-11-02 00:31:05 -0300

    typo

--- utils/commands/challenge.py
+++ utils/commands/challenge.py
@@ -3,7 +3,9 @@ import utils.logging.log as log
 from ..database.setup import Challenge
 
 def get_challenges():
-    # pega todas as challs
+    """
+    Essa funcao retorna todos os challenges cadastrados no CTF
+    """
     try:
         challenges = Challenge.select(
             Challenge.id,
@@ -16,11 +18,10 @@ def get_challenges():
         challs = ""
         for challenge in challenges.iterator(): 
             challs += f'''{challenge.name} ({challenge.id}) - {challenge.points} Pontos - {challenge.category}
-{challenge.description}
-{challenge.url}
-------------------------------------------------------------
-'''
+                          {challenge.description}
+                          {challenge.url}
+                       ------------------------------------------------------------
+                       '''
         return challs
-
     except Exception as err:
         log.err(err)
