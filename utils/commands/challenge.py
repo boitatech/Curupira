d96a6796c9391f33302843924384dd898c4f7144
Diego Pisani <dpisani@gavea.com>
2021-11-02 19:07:18 -0300

    cleaning mess in get_challenge()

--- utils/commands/challenge.py
+++ utils/commands/challenge.py
@@ -29,11 +29,11 @@ def get_challenges(ctx):
 
         challs = ""
         for challenge in challenges.iterator():
-            if len(attempts) > 0:
-                # for attempt in attempts.iterator():
-                print(f"<> <> <>Attempt: {attempt}")
-                # if challenge.id == attempt.chall_id:
-                print('O USER JA FEZ A CHALL')
+            if len(attempts) >= 1:
+                for attempt in attempts.iterator():
+                    print(f"<> <> <>Attempt: {attempt}")
+                    if challenge.id == attempt.chall_id:
+                        print('O USER JA FEZ A CHALL')
             challs += f'''{challenge.name} ({challenge.id}) - {challenge.points} Pontos - {challenge.category}
                           {challenge.description}
                           {challenge.url}
