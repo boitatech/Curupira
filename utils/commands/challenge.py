c89c776f69e2bc8dd519459f390bb37c96394064
Luidiblu <diego.luidiblu@gmail.com>
2021-11-02 02:48:27 -0300

    wip

--- utils/commands/challenge.py
+++ utils/commands/challenge.py
@@ -8,28 +8,30 @@ def get_challenges(ctx):
     Essa funcao retorna todos os challenges cadastrados no CTF
     """
     try:
-        print('vai pegar o user')
+        print('=====> vai pegar o user')
         user = User.get(User.discordId == ctx.author.id)
-        print('terminou de pegar o user')
+        print('=====> terminou de pegar o user')
         
-        print('vai pegar a challenge')
+        print('=====> vai pegar a challenge')
         challenges = Challenge.select(Challenge.id,
                                       Challenge.name, 
                                       Challenge.points, 
                                       Challenge.category, 
                                       Challenge.description,
                                       Challenge.url)
-        print('terminou de pegar a challenge')
+        print('=====> terminou de pegar a challenge')
         
-        print('vai pegar a attempt')
+        print('=====> vai pegar a attempt')
         attempts = Attempt.select(Attempt.chall_id).where(Attempt.correct == True and Attempt.user_id == user.id)
-        print('terminou de pegar a attempt')
+        print('=====> terminou de pegar a attempt')
 
         challs = ""
         for challenge in challenges.iterator():
-            for attempt in attempts.iterator():
-                if challenge.id == attempt.chall_id:
-                    print('O USER JA FEZ A CHALL')
+            if len(attempts) > 0:
+                for attempt in attempts.iterator():
+                    print(f"<> <> <>Attempt: {attempt}")
+                    if challenge.id == attempt.chall_id:
+                        print('O USER JA FEZ A CHALL')
             challs += f'''{challenge.name} ({challenge.id}) - {challenge.points} Pontos - {challenge.category}
                           {challenge.description}
                           {challenge.url}
