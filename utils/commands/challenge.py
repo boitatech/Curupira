8c6b0e05eb7adc3a327cae49df2e947d137e7f3a
Diego Pisani <dpisani@gavea.com>
2021-11-02 21:47:40 -0300

    WIP

--- utils/commands/challenge.py
+++ utils/commands/challenge.py
@@ -24,7 +24,7 @@ def get_challenges(ctx):
         print('=====> terminou de pegar a challenge')
 
         print('=====> vai pegar a attempt')
-        attempts = Attempt.select().where(Attempt.correct == True, Attempt.user_id == user.id)
+        attempts = Attempt.select(Attempt.chall_id).where(Attempt.correct == True, Attempt.user_id == user.id)
         print('=====> terminou de pegar a attempt')
 
         challs = ""
