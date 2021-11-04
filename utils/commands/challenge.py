b28e7f88a3fbcfe964044f50d922a292f0faacf9
Diego Pisani <dpisani@gavea.com>
2021-11-04 12:46:36 -0300

    fixing embed

--- utils/commands/challenge.py
+++ utils/commands/challenge.py
@@ -36,14 +36,14 @@ def get_challenges(ctx):
                             _banned_ids.append(challenge.id)
 
                 if challenge.id not in _banned_ids:
-                    challs_list.append(f'''
-                                            **[ID: {challenge.id}] - {challenge.name}**
-                                            [CATEGORY] - {challenge.category}
-                                            [PONTOS] - {challenge.points}
-                                            [DESCRIPTION] - {challenge.description}
-                                            [+] {challenge.url}
-                                            {"-" * 64}
-                                        ''')
+                    challs_list.append(discord.Embed(title=f"{challenge.name}", description=f'''
+                                                                        **[ID: {challenge.id}] - {challenge.name}**
+                                                                        [CATEGORY] - {challenge.category}
+                                                                        [PONTOS] - {challenge.points}
+                                                                        [DESCRIPTION] - {challenge.description}
+                                                                        [+] {challenge.url}
+                                                                        {"-" * 64}
+                                                                    '''))
             print(challs_list)
             return challs_list
         except Exception as err:
