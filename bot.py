40e31d23aef94ef03b213eead5e7818c559fca75
Diego Pisani <dpisani@gavea.com>
2021-11-04 00:09:26 -0300

    wip

--- bot.py
+++ bot.py
@@ -59,26 +59,29 @@ async def register(ctx):
     """
     Registra o usuário
     """
-    if isinstance(ctx.channel, discord.channel):
-        def check(reaction, user):
-            return user == ctx.author and (str(reaction.emoji) == '\N{Cross Mark}' or str(reaction.emoji) == '\N{White Heavy Check Mark}')
+    def check(reaction, user):
+        return user == ctx.author and (str(reaction.emoji) == '\N{Cross Mark}' or str(reaction.emoji) == '\N{White Heavy Check Mark}')
 
-        userID = ctx.author.id
-        message = await ctx.send(f'<@{userID}> gostaria de se registrar?')
+    await ctx.author.create_dm()
+    if isinstance(ctx.channel, discord.channel.DMChannel):
+        try:
+            userID = ctx.author.id
+            message = await ctx.send(f'<@{userID}> gostaria de se registrar?')
 
-        symbols = ['\N{White Heavy Check Mark}', '\N{Cross Mark}']
-        for symbol in symbols:
-            await message.add_reaction(symbol)
-        reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
-        if reaction.emoji == '\N{White Heavy Check Mark}':
-            await ctx.send(register_user(userID))
+            symbols = ['\N{White Heavy Check Mark}', '\N{Cross Mark}']
+            for symbol in symbols:
+                await message.add_reaction(symbol)
+            reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+            if reaction.emoji == '\N{White Heavy Check Mark}':
+                await ctx.send(register_user(userID))
             await ctx.message.delete()
             await message.delete()
-        elif reaction.emoji == '\N{Cross Mark}':
-            await ctx.message.delete()
-            await message.delete()
+        except Exception as err:
+            log.err(err)
     else:
-        await ctx.author.dm_channel.send("Você precisa se registrar lá no servidor da Boitatech! ;-)")
+        await ctx.message.delete()
+        await ctx.author.dm_channel.send("Utilize o comando `$register` aqui!")
+
 
 
 @bot.command()
