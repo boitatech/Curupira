0d02b163a74a4b44fd1a1c3a484358f5c78aaa45
Diego Pisani <dpisani@gavea.com>
2021-11-04 00:25:04 -0300

    wip

--- bot.py
+++ bot.py
@@ -72,6 +72,7 @@ async def register(ctx):
                 await message.add_reaction(symbol)
             reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
             if reaction.emoji == '\N{White Heavy Check Mark}':
+                print("reagiu com check")
                 await ctx.author.dm_channel.send(register_user(userID))
             # await ctx.message.delete()
             await message.delete()
