b3095fffb74b06d64983c3d57695c35aa20b412c
Diego Pisani <dpisani@gavea.com>
2021-11-03 23:28:27 -0300

    wip

--- utils/commands/rank.py
+++ utils/commands/rank.py
@@ -8,26 +8,24 @@ def get_ranking_top_ten(ctx):
     """
     Essa funcao retorna os top 10 colocados no CTF
     """
-    user = User.get(User.discordId == ctx.author.id)
-    print(user)
-    print("888")
-    print(dir(user))
-    print("888")
-    print(type(user))
-    try:
-        users = (
-            User.select().order_by(User.score.desc(), User.last_submit.asc())
-                         .limit(10)
-        )
-        ranking = "".join(
-            f'#{idx+1} <@{user.discordId}> - {user.score}\n'
-            for idx, user in enumerate(users.iterator())
-        )
-        rank_expr = peewee.fn.rank().over(order_by=[User.score.desc(), User.last_submit.asc()])
-        subquery = User.select(User.discordId, rank_expr.alias('rank'))
-        user_rank = peewee.Select(columns=[subquery.c.discordId, subquery.c.rank]).from_(subquery).where(subquery.c.discordId == str(ctx.author.id)).bind(_DB).first()
-        embed = discord.Embed(title="Ranking", description=ranking)
-        embed.set_footer(text=f'Sua posição é #{user_rank["rank"]}' if user_rank else 'Registre-se digitando $register')
-        return embed
-    except Exception as err:
-        log.err(err)
+    user = User.get_or_none(User.discordId == ctx.author.id)
+    if user is None:
+        return discord.Embed(title="Regitre-se", description="Use $register para se registrar")
+    else:
+        try:
+            users = (
+                User.select().order_by(User.score.desc(), User.last_submit.asc())
+                            .limit(10)
+            )
+            ranking = "".join(
+                f'#{idx+1} <@{user.discordId}> - {user.score}\n'
+                for idx, user in enumerate(users.iterator())
+            )
+            rank_expr = peewee.fn.rank().over(order_by=[User.score.desc(), User.last_submit.asc()])
+            subquery = User.select(User.discordId, rank_expr.alias('rank'))
+            user_rank = peewee.Select(columns=[subquery.c.discordId, subquery.c.rank]).from_(subquery).where(subquery.c.discordId == str(ctx.author.id)).bind(_DB).first()
+            embed = discord.Embed(title="Ranking", description=ranking)
+            embed.set_footer(text=f'Sua posição é #{user_rank["rank"]}' if user_rank else 'Registre-se digitando $register')
+            return embed
+        except Exception as err:
+            log.err(err)
