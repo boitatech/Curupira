8e487aad19e04a9db73d752a325e99733b7344b5
Diego Pisani <dpisani@gavea.com>
2021-11-03 23:33:10 -0300

    wip

--- utils/commands/challenge.py
+++ utils/commands/challenge.py
@@ -7,38 +7,42 @@ def get_challenges(ctx):
     """
     Essa funcao retorna todos os challenges cadastrados no CTF
     """
-    try:
-        user = User.get(User.discordId == ctx.author.id)
+    user = User.get_or_none(User.discordId == ctx.author.id)
+    if user is None:
+        return discord.Embed(title="Regitre-se", description="Use $register para se registrar")
+    else:
+        try:
+            user = User.get(User.discordId == ctx.author.id)
 
-        challenges = Challenge.select(
-                                    Challenge.id,
-                                    Challenge.name,
-                                    Challenge.points,
-                                    Challenge.category,
-                                    Challenge.description,
-                                    Challenge.url
-                                    )
+            challenges = Challenge.select(
+                                        Challenge.id,
+                                        Challenge.name,
+                                        Challenge.points,
+                                        Challenge.category,
+                                        Challenge.description,
+                                        Challenge.url
+                                        )
 
-        attempts = Attempt.select(Attempt.chall_id).where(Attempt.correct == True, Attempt.user_id == user.id)
+            attempts = Attempt.select(Attempt.chall_id).where(Attempt.correct == True, Attempt.user_id == user.id)
 
-        challs = ""
+            challs = ""
 
-        for challenge in challenges:
-            _banned_ids = []
-            if len(attempts) >= 1:
-                for attempt in attempts:
-                    if challenge.id == attempt.chall_id_id:
-                        _banned_ids.append(challenge.id)
+            for challenge in challenges:
+                _banned_ids = []
+                if len(attempts) >= 1:
+                    for attempt in attempts:
+                        if challenge.id == attempt.chall_id_id:
+                            _banned_ids.append(challenge.id)
 
-            if challenge.id not in _banned_ids:
-                challs += f'''
-                              **[ID: {challenge.id}] - {challenge.name}**
-                              [CATEGORY] - {challenge.category}
-                              [PONTOS] - {challenge.points}
-                              [DESCRIPTION] - {challenge.description}
-                              [+] {challenge.url}
-                              {"-" * 64}
-                        '''
-        return discord.Embed(title="Challenges", description=challs)
-    except Exception as err:
-        log.err(err)
+                if challenge.id not in _banned_ids:
+                    challs += f'''
+                                **[ID: {challenge.id}] - {challenge.name}**
+                                [CATEGORY] - {challenge.category}
+                                [PONTOS] - {challenge.points}
+                                [DESCRIPTION] - {challenge.description}
+                                [+] {challenge.url}
+                                {"-" * 64}
+                            '''
+            return discord.Embed(title="Challenges", description=challs)
+        except Exception as err:
+            log.err(err)
--- utils/commands/flag.py
+++ utils/commands/flag.py
@@ -14,30 +14,34 @@ def check_flag(challId, flag, discordId):
     :flag => flag do Challenge
     :userId => ID do Usuario
     """
-    try:
-        chall = Challenge.get_by_id(challId)
-    except Exception as e:
-        log.err(e)
-        return "Esse challenge id não existe!"
-
-    try:
-        Attempt.get(Attempt.correct == True, Attempt.user_id == user.id, Attempt.chall_id == challId)
-        return "Você já submeteu a flag desse desafio!"
-    except peewee.DoesNotExist as e:
-        log.err(e)
-
-    if chall.flag == flag:
-        try:
-            Attempt.create(user_id=user.id, chall_id=challId, flag=flag, correct=True, timestamp=datetime.timestamp(datetime.now()))
-            User.update(score=user.score + chall.points, last_submit=datetime.timestamp(datetime.now())).where(User.discordId == discordId).execute()
-            return "Flag correta!"
-        except Exception as e:
-            log.err(e)
-            return "Erro ao criar Attempt True"
+    user = User.get_or_none(User.discordId == ctx.author.id)
+    if user is None:
+        return discord.Embed(title="Regitre-se", description="Use $register para se registrar")
     else:
         try:
-            Attempt.create(user_id=user.id, chall_id=challId, flag=flag, correct=False, timestamp=datetime.timestamp(datetime.now()))
-            return "Flag incorreta!"
+            chall = Challenge.get_by_id(challId)
         except Exception as e:
             log.err(e)
-            return "Erro ao criar Attempt False"
+            return "Esse challenge id não existe!"
+
+        try:
+            Attempt.get(Attempt.correct == True, Attempt.user_id == user.id, Attempt.chall_id == challId)
+            return "Você já submeteu a flag desse desafio!"
+        except peewee.DoesNotExist as e:
+            log.err(e)
+
+        if chall.flag == flag:
+            try:
+                Attempt.create(user_id=user.id, chall_id=challId, flag=flag, correct=True, timestamp=datetime.timestamp(datetime.now()))
+                User.update(score=user.score + chall.points, last_submit=datetime.timestamp(datetime.now())).where(User.discordId == discordId).execute()
+                return "Flag correta!"
+            except Exception as e:
+                log.err(e)
+                return "Erro ao criar Attempt True"
+        else:
+            try:
+                Attempt.create(user_id=user.id, chall_id=challId, flag=flag, correct=False, timestamp=datetime.timestamp(datetime.now()))
+                return "Flag incorreta!"
+            except Exception as e:
+                log.err(e)
+                return "Erro ao criar Attempt False"
