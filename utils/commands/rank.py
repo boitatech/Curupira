7ed049440c19a05e1cddc348a9d3eac273c795ac
Diego Pisani <dpisani@gavea.com>
2021-11-05 13:05:08 -0300

    WIP

--- utils/commands/rank.py
+++ utils/commands/rank.py
@@ -26,6 +26,7 @@ def scoreboard():
         User.select().order_by(User.score.desc(), User.last_submit.asc())
                         .limit(10)
     )
+
     ranking = "".join(
         f'#{idx+1} <@{user.discordId}> - {user.score}\n'
         for idx, user in enumerate(users.iterator())
@@ -34,4 +35,4 @@ def scoreboard():
     channel = bot.get_channel(906198041814507541)
     message = channel.fetch_message(906201505793703967)
 
-    message.edit(content=ranking)
+    message.edit(content=discord.Embed(description=ranking))
